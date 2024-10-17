73857537073414390ff901b8f98e936601c79fbd
Igor Kohsin <igor.kohsin@icloud.com>
2024-10-17 12:39:39 +0200

    Initial commit

---
+++ src/main/java/uni/studysmart/StudySmartApplication.java
@@ -0,0 +1,13 @@
+package uni.studysmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySmartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySmartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uni/studysmart/StudySmartApplicationTests.java
@@ -0,0 +1,13 @@
+package uni.studysmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySmartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
