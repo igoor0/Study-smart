43a2ef6cd7a179686160efb0b62c825c38c5dd95
Igor Kohsin <igor.kohsin@icloud.com>
2024-10-31 11:42:04 +0100

    Minor changes in auth package

--- src/main/java/uni/studysmart/auth/AuthenticationService.java
+++ src/main/java/uni/studysmart/auth/AuthenticationService.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service;
 import uni.studysmart.config.JwtService;
 import uni.studysmart.exception.ApiRequestException;
 import uni.studysmart.model.Role;
+import uni.studysmart.model.Student;
+import uni.studysmart.model.Lecturer;
 import uni.studysmart.model.User;
 import uni.studysmart.repository.UserRepository;
 
@@ -21,21 +23,41 @@ public class AuthenticationService {
     private final AuthenticationManager authenticationManager;
 
     public AuthenticationResponse register(RegisterRequest request) {
-        var user = User.builder()
-                .firstname(request.getFirstname())
-                .lastname(request.getLastname())
-                .email(request.getEmail())
-                .password(passwordEncoder.encode(request.getPassword()))
-                .role(Role.USER)
-                .build();
-        if(userRepository.findByEmail(request.getEmail()).isPresent()){
+        validateEmailForRole(request.getEmail());
+
+        if (userRepository.findByEmail(request.getEmail()).isPresent()) {
             throw new ApiRequestException("User with email " + request.getEmail() + " already exists");
-        }else {
-            userRepository.save(user);
         }
-        var jwtToken = jwtService.generateToken(user);
+
+        // Tworzymy użytkownika na podstawie roli określonej przez e-mail
+        User user;
+        if (request.getEmail().endsWith("@student.umg.pl")) {
+            user = new Student();
+            user.setRole(Role.STUDENT);
+        } else if (request.getEmail().endsWith("@wykladowca.umg.pl")) {
+            user = new Lecturer();
+            user.setRole(Role.LECTURER);
+        } else if (request.getEmail().endsWith("@planner.umg.pl")) {
+            user = new User();
+            user.setRole(Role.PLANNER);
+        } else if (request.getEmail().endsWith("@admin.umg.pl")) {
+            user = new User();
+            user.setRole(Role.ADMIN);
+        } else {
+            throw new ApiRequestException("Invalid email domain specified");
+        }
+
+        user.setEmail(request.getEmail());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+
+        // Zapis użytkownika do bazy danych
+        userRepository.save(user);
+
+        // Generowanie tokenu JWT
+        var jwtToken = jwtService.generateToken(user, user.getId());
         return AuthenticationResponse.builder()
                 .token(jwtToken)
+                .id(user.getId())
                 .build();
     }
 
@@ -45,24 +67,33 @@ public class AuthenticationService {
                         request.getEmail(),
                         request.getPassword())
         );
-        var user = userRepository.findByEmail(request.getEmail()).orElseThrow(() -> new ApiRequestException("User with the email " + request.getEmail() + " not found"));
-        var jwtToken = jwtService.generateToken(user);
-        return  AuthenticationResponse.builder()
+        var user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new ApiRequestException("User with the email " + request.getEmail() + " not found"));
+        var jwtToken = jwtService.generateToken(user, user.getId());
+        return AuthenticationResponse.builder()
                 .token(jwtToken)
+                .id(user.getId())
                 .build();
     }
 
     public AuthenticationResponse changePassword(ChangePasswordRequest changePasswordRequest) {
-        var user = userRepository.findByEmail(changePasswordRequest.getEmail()).orElseThrow(() -> new ApiRequestException("User with the email " + changePasswordRequest.getEmail() + " not found"));
-        if(passwordEncoder.matches(changePasswordRequest.getOldPassword(), user.getPassword())){
+        var user = userRepository.findByEmail(changePasswordRequest.getEmail())
+                .orElseThrow(() -> new ApiRequestException("User with the email " + changePasswordRequest.getEmail() + " not found"));
+        if (passwordEncoder.matches(changePasswordRequest.getOldPassword(), user.getPassword())) {
             user.setPassword(passwordEncoder.encode(changePasswordRequest.getNewPassword()));
             userRepository.save(user);
-            var jwtToken = jwtService.generateToken(user);
+            var jwtToken = jwtService.generateToken(user, user.getId());
             return AuthenticationResponse.builder()
                     .token(jwtToken)
                     .build();
-        }else {
+        } else {
             throw new ApiRequestException("Old password is incorrect");
+        }
+    }
+
+    private void validateEmailForRole(String email) {
+        if (!email.endsWith("@student.umg.pl") && !email.endsWith("@wykladowca.umg.pl") && !email.endsWith("@planner.umg.pl") && !email.endsWith("@admin.umg.pl")) {
+            throw new ApiRequestException("Mail domain has to end with either: student/wykladowca/planista @umg.pl");
         }
     }
 }
