0b743f0d3e5e2bb9343f4d3cad09306b7cfef046
Igor Kohsin <igor.kohsin@icloud.com>
2024-12-08 00:59:11 +0100

    Refactor due to lack of logic there

--- src/main/java/uni/studysmart/controller/ScheduleController.java
+++ src/main/java/uni/studysmart/controller/ScheduleController.java
@@ -1,6 +1,7 @@
 package uni.studysmart.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import uni.studysmart.service.ScheduleService;
@@ -12,43 +13,5 @@ import java.util.List;
 @RequestMapping("/api/schedules")
 public class ScheduleController {
 
-    @Autowired
-    private ScheduleService scheduleService;
-
-    /**
-     * Endpoint do uzyskiwania najlepszego harmonogramu zajęć dla danej grupy, kursu i wykładowcy.
-     *
-     * @param lecturerId ID wykładowcy
-     * @param courseId   ID kursu
-     * @param groupId    ID grupy
-     * @return Lista najlepszych godzin zajęć
-     */
-    @GetMapping("/best")
-    public ResponseEntity<List<LocalTime>> getBestSchedule(
-            @RequestParam Long lecturerId,
-            @RequestParam Long courseId,
-            @RequestParam Long groupId) {
-
-        List<LocalTime> bestTimes = scheduleService.getBestMatchingSchedule(lecturerId, courseId, groupId);
-        return ResponseEntity.ok(bestTimes);
-    }
-
-    /**
-     * Endpoint do zapisywania harmonogramu zajęć dla danej grupy, kursu i wykładowcy.
-     *
-     * @param lecturerId ID wykładowcy
-     * @param courseId   ID kursu
-     * @param groupId    ID grupy
-     * @return Status operacji
-     */
-    @PostMapping("/save")
-    public ResponseEntity<String> saveSchedule(
-            @RequestParam Long lecturerId,
-            @RequestParam Long courseId,
-            @RequestParam Long groupId) {
-
-        scheduleService.saveSchedule(lecturerId, courseId, groupId);
-        return ResponseEntity.ok("Harmonogram zapisany pomyślnie");
-    }
 }
 
