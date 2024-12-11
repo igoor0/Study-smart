dba911b0fab8380ba01bbd0844272f839bbabd8c
Igor Kohsin <igor.kohsin@icloud.com>
2024-12-11 23:29:15 +0100

    Fix ApiExceptionHandler for frontend debug

--- src/main/java/uni/studysmart/exception/ApiExceptionHandler.java
+++ src/main/java/uni/studysmart/exception/ApiExceptionHandler.java
@@ -4,12 +4,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
 @ControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
     @ExceptionHandler(value = {ApiRequestException.class})
     public ResponseEntity<Object> handleApiRequestException(ApiRequestException e) {
         HttpStatus badRequest = HttpStatus.BAD_REQUEST;
@@ -17,8 +19,21 @@ public class ApiExceptionHandler {
         ApiException apiException = new ApiException(
                 e.getMessage(),
                 badRequest,
-                ZonedDateTime.now(ZoneId.of("ECT"))
+                ZonedDateTime.now(ZoneId.of("UTC")),
+                "API_REQUEST_ERROR"
         );
         return new ResponseEntity<>(apiException, badRequest);
+    }
+    @ExceptionHandler(value = {NullPointerException.class})
+    public ResponseEntity<Object> handleNullPointerException(NullPointerException e) {
+        HttpStatus internalServerError = HttpStatus.INTERNAL_SERVER_ERROR;
+
+        ApiException apiException = new ApiException(
+                e.getMessage(),
+                internalServerError,
+                ZonedDateTime.now(ZoneId.of("ECT")),
+                "NULL_POINTER_EXCEPTION"
+        );
+        return new ResponseEntity<>(apiException, internalServerError);
     }
 }
