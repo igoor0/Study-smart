bae2a64d3c292a546cc304648924ca34e9bbd069
Igor Kohsin <igor.kohsin@icloud.com>
2024-12-11 22:59:20 +0100

    Initialize isScheduled value

--- src/main/java/uni/studysmart/model/Course.java
+++ src/main/java/uni/studysmart/model/Course.java
@@ -18,7 +18,7 @@ public class Course {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    private Boolean isScheduled; // Czy dany kurs jest już zaplanowany (posiada już wybrane godziny w tygodniu) TODO: czy jest to odpowiednie miejsce na taką zmienną?
+    private Boolean isScheduled = false; // Czy dany kurs jest już zaplanowany (posiada już wybrane godziny w tygodniu) TODO: czy jest to odpowiednie miejsce na taką zmienną?
     private Integer courseDuration;
     private LocalTime startTime;  // Godzina rozpoczęcia danego kursu
     private LocalTime endTime;  // Godzina zakończenia danego kursu
