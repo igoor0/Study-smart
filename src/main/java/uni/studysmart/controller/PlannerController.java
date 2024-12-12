1936d2ae453f90130ee7202a0c8a7f255f735e0e
Igor Kohsin <igor.kohsin@icloud.com>
2024-12-12 18:51:57 +0100

    Refactor endpoints

--- src/main/java/uni/studysmart/controller/PlannerController.java
+++ src/main/java/uni/studysmart/controller/PlannerController.java
@@ -9,29 +9,4 @@ import uni.studysmart.service.PlannerService;
 @RequestMapping("api/planners")
 public class PlannerController {
 
-    @Autowired
-    private PlannerService plannerService;
-
-    @PostMapping("/add/{studentId}/to/{groupId}")
-    public ResponseEntity<String> addStudentToGroup(@PathVariable Long studentId, @PathVariable Long groupId) {
-        return plannerService.addStudentToGroup(studentId, groupId);
-    }
-
-    @PutMapping("/removeStudentFromGroup")
-    public ResponseEntity<String> deleteStudentFromGroup(@RequestParam Long studentId, @RequestParam Long groupId) {
-        return plannerService.removeStudentFromGroup(studentId, groupId);
-    }
-
-    @PostMapping("/addLecturerToCourse")
-    public ResponseEntity<String> addLecturerToCourse(@RequestParam Long lecturerId, @RequestParam Long courseId) {
-        return plannerService.addLecturerToCourse(lecturerId, courseId);
-    }
-
-    @PutMapping("/removeLecturerFromCourse")
-    public ResponseEntity<String> deleteLecturerFromCourse(@RequestParam Long courseId) {
-        return plannerService.removeLecturerFromCourse(courseId);
-    }
-
-
-
 }
--- src/main/java/uni/studysmart/service/PlannerService.java
+++ src/main/java/uni/studysmart/service/PlannerService.java
@@ -28,65 +28,6 @@ public class PlannerService {
         this.groupRepository = groupRepository;
     }
 
-    public ResponseEntity<String> addStudentToGroup(Long studentId, Long groupId) {
-        Group group = groupRepository.findById(groupId).orElse(null);
-        if (group == null) {
-            return ResponseEntity.notFound().build();
-        }
-        Student student = studentRepository.findById(studentId).orElse(null);
-        if (student == null) {
-            return ResponseEntity.notFound().build();
-        }
-        group.getStudents().add(student);
-        student.setGroup(group);
-        groupRepository.save(group);
-        studentRepository.save(student);
-        return ResponseEntity.ok("Student został dodany do grupy");
-    }
-
-    public ResponseEntity<String> removeStudentFromGroup(Long studentId, Long groupId) {
-        Group group = groupRepository.findById(groupId).orElse(null);
-        if (group == null) {
-            return ResponseEntity.notFound().build();
-        }
-        Student student = studentRepository.findById(studentId).orElse(null);
-        if (student == null) {
-            return ResponseEntity.notFound().build();
-        }
-        group.getStudents().remove(student);
-        student.setGroup(null);
-        groupRepository.save(group);
-        studentRepository.save(student);
-        return ResponseEntity.ok("Student został usunięty z grupy");
-    }
-
-
-
-    public ResponseEntity<String> addLecturerToCourse(Long courseId, Long lecturerId) {
-        Course course = courseRepository.findById(courseId).orElse(null);
-        if (course == null) {
-            return ResponseEntity.notFound().build();
-        }
-        Lecturer lecturer = lecturerRepository.findById(lecturerId).orElse(null);
-        if (lecturer == null) {
-            return ResponseEntity.notFound().build();
-        }
-        course.setLecturer(lecturer);
-        lecturer.getCourses().add(course);
-        lecturerRepository.save(lecturer);
-        courseRepository.save(course);
-        return ResponseEntity.ok("Wykładowca został dodany do kursu");
-    }
-
-    public ResponseEntity<String> removeLecturerFromCourse(Long courseId) {
-        Course course = courseRepository.findById(courseId).orElse(null);
-        if (course == null) {
-            return ResponseEntity.notFound().build();
-        }
-        course.setLecturer(null);
-        courseRepository.save(course);
-        return ResponseEntity.ok("Wykładowca został usunięty z kursu");
-    }
 
 
     //TODO CRUD DO SCHEDULE
