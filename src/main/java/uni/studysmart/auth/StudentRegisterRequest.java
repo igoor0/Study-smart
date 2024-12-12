941eb70d4e3b38b21f7eddbfe56b22ab6a1fa925
Igor Kohsin <igor.kohsin@icloud.com>
2024-12-12 17:47:01 +0100

    Add constructors manually

--- src/main/java/uni/studysmart/auth/StudentRegisterRequest.java
+++ src/main/java/uni/studysmart/auth/StudentRegisterRequest.java
@@ -3,12 +3,19 @@ package uni.studysmart.auth;
 import lombok.*;
 
 @EqualsAndHashCode(callSuper = true)
-@AllArgsConstructor
-@NoArgsConstructor
 @Data
 @Getter
+@NoArgsConstructor
 public class StudentRegisterRequest extends RegisterRequest {
     private String indexNumber;
     private String major;
+
+    public StudentRegisterRequest(String firstName, String lastName, String mail, String password, String major) {
+        super(firstName, lastName, mail, password);
+        this.major = major;
+    }
+    public StudentRegisterRequest(String firstName, String lastName, String email, String password) {
+        super(firstName, lastName, email, password);
+    }
 
 }
