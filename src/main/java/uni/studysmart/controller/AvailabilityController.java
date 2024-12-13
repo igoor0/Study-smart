f5456525e1d081e35ade67114f61d122fcd41970
Igor Kohsin <igor.kohsin@icloud.com>
2024-12-13 21:34:32 +0100

    fix small mistakes

--- src/main/java/uni/studysmart/controller/AvailabilityController.java
+++ src/main/java/uni/studysmart/controller/AvailabilityController.java
@@ -24,28 +24,22 @@ public class AvailabilityController {
         Long createdId = availabilityService.addAvailability(availabilityDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdId);
     }
-//    @GetMapping
-//    public ResponseEntity<List<AvailabilityDTO>> getAvailabilities() {
-//        List<AvailabilityDTO> availibilityDTOList = availabilityService.getAllAvailabilities();
-//        return ResponseEntity.ok(availibilityDTOList);
-//    }
-//
-//    @PostMapping
-//    public ResponseEntity<Long> createAvailability(@RequestBody AvailabilityDTO availabilityDTO) {
-//        Long createdId = availabilityService.addAvailability(availabilityDTO);
-//        return ResponseEntity.status(HttpStatus.CREATED).body(createdId);
-//    }
-//
-//    @GetMapping("/{id}")
-//    public ResponseEntity<AvailabilityDTO> getAvailabilityById(@PathVariable Long id) {
-//        AvailabilityDTO availabilityDTO = availabilityService.getAvailabilityById(id);
-//        return ResponseEntity.ok(availabilityDTO);
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public ResponseEntity<Void> deleteAvailability(@PathVariable Long id) {
-//        availabilityService.deleteAvailability(id);
-//        return ResponseEntity.noContent().build();
-//    }
+    @GetMapping
+    public ResponseEntity<List<AvailabilityDTO>> getAvailabilities() {
+        List<AvailabilityDTO> availibilityDTOList = availabilityService.getAllAvailabilities();
+        return ResponseEntity.ok(availibilityDTOList);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AvailabilityDTO> getAvailabilityById(@PathVariable Long id) {
+        AvailabilityDTO availabilityDTO = availabilityService.getAvailabilityById(id);
+        return ResponseEntity.ok(availabilityDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAvailability(@PathVariable Long id) {
+        availabilityService.deleteAvailability(id);
+        return ResponseEntity.noContent().build();
+    }
 }
 
