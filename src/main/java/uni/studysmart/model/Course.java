bc2d1cfafe11746988932076125327876ea733e2
Igor Kohsin <igor.kohsin@icloud.com>
2024-12-14 15:54:54 +0100

    Minor change

--- src/main/java/uni/studysmart/model/Course.java
+++ src/main/java/uni/studysmart/model/Course.java
@@ -17,23 +17,11 @@ public class Course {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    private Boolean isScheduled = false; // Czy dany kurs jest już zaplanowany (posiada już wybrane godziny w tygodniu)
+    private Boolean isScheduled = false;
     private Integer courseDuration;
-    private LocalTime startTime;  // Godzina rozpoczęcia danego kursu
-    private LocalTime endTime;  // Godzina zakończenia danego kursu
+    private LocalTime startTime;
+    private LocalTime endTime;
 
     @ManyToOne
     private Lecturer lecturer;
-
-    //Mamy dany kurs
-    //Ten kurs jest prowadzony przez KILKU prowadzących (dany kurs może mieć wielu prowadzących)
-    //Ten kurs ma wiele grup studentów (kilka grup studentów może mieć ten sam przedmiot)
-    //Ten kurs ma określoną ilość godzin trwania, z założeniem na cały semestr
-    //Dany kurs może wystąpić dla danej grupy tylko raz w tygodniu, przy założeniu, że trwa cały semestr (powiedzmy 15 tygodni)
-    //
-
-    //CO MOŻEMY ZROBIC:
-    // - możemy dodać nowy kurs
-    // - możemy usunąc kurs
-    // - możemy
 }
